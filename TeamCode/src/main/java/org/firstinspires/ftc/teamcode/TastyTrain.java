da7244abf392b07b38166eaeb11ce3b62392fb65
Migala <amigala@micds.org>
2017-09-07 17:05:57 -0500

    Initial setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TastyTrain.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by amigala on 9/7/2017.
+ */
+
+public class TastyTrain extends OpMode {
+
+    public void init() {
+
+    }
+
+    public void loop() {
+
+    }
+}
