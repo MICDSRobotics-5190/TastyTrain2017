06a13ba2f4e0c893abe50d5288e1f52914fe6428
Migala <amigala@micds.org>
2017-09-07 17:12:34 -0500

    Basic teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TastyTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TastyTrain.java
@@ -1,18 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by amigala on 9/7/2017.
+ * TastyTrain
+ * @author Alex Migala
+ * @since 9/7/17
  */
-
 public class TastyTrain extends OpMode {
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+    private Servo leftServo;
+    private Servo rightServo;
 
     public void init() {
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
 
+        leftServo = hardwareMap.servo.get("left_servo");
+        rightServo = hardwareMap.servo.get("right_servo");
     }
 
     public void loop() {
-
+        leftMotor.setPower(gamepad1.left_stick_y);
+        rightMotor.setPower(gamepad2.right_stick_y);
     }
 }
