aa6c85fb8a81725a8aa610c0475c34b614f3ebd7
Migala <amigala@micds.org>
2017-09-07 17:42:55 -0500

    Compiler annotation and some servo stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TastyTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TastyTrain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -9,6 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * @author Alex Migala
  * @since 9/7/17
  */
+@TeleOp(name = "Tasty Train", group = "Driver")
 public class TastyTrain extends OpMode {
     private DcMotor leftMotor;
     private DcMotor rightMotor;
@@ -18,6 +20,8 @@ public class TastyTrain extends OpMode {
     public void init() {
         leftMotor = hardwareMap.dcMotor.get("left_motor");
         rightMotor = hardwareMap.dcMotor.get("right_motor");
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         leftServo = hardwareMap.servo.get("left_servo");
         rightServo = hardwareMap.servo.get("right_servo");
@@ -26,5 +30,10 @@ public class TastyTrain extends OpMode {
     public void loop() {
         leftMotor.setPower(gamepad1.left_stick_y);
         rightMotor.setPower(gamepad2.right_stick_y);
+
+        if (gamepad1.a) {
+            leftServo.setPosition(1);
+            rightServo.setPosition(1);
+        }
     }
 }
